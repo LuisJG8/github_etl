c4671be9ae5abfb09b0796924d4826c6b756df14
Luis J Gonzalez <luis.glez2015@yahoo.com>
2025-10-26 19:53:22 -0400

    Add initial project structure with Docker, Docker Compose, and Celery integration

---
+++ client.py
@@ -0,0 +1,20 @@
+import time
+from celery.result import AsyncResult
+from worker import random_number, app 
+
+time.sleep(5)
+
+result_future = random_number.delay(100)
+result = AsyncResult(result_future.id, app=app)
+
+print('Done')
+
+print(result.state) 
+
+while True:
+    if result.ready():
+        print(result.get())
+        break
+    else:
+        print(result.state)
+        time.sleep(1)
--- main.py
+++ main.py
@@ -1,25 +1,29 @@
+import os 
 import asyncio
-import aiohttp
 import time
+from datetime import datetime
 from github import Auth, Github
+from dotenv import load_dotenv
+load_dotenv()
 
-
-api_token =
+api_token = os.getenv("GITHUB_API_TOKEN")
+print(api_token)
 auth = Auth.Token(api_token)
 gh = Github(auth=auth)
 
-some = gh.get_repo("Netflix/Maestro")
+time_before = datetime.now()
 
-async def giting_data():
+repositories = gh.get_repos(since=0)
 
-    async with aiohttp.ClientSession() as session:
-        async with some as response:
+counter = 0
+for repo in repositories:
+    counter += 1 
+    print((repo))
+    print(repo.stargazers_count)
 
-            print("Status: ", response.status)
+    if counter == 5:
+        break
 
+time_after = datetime.now()
 
-
-print(repositories.stargazers_count)
-      
-# rep = gh.get_repos()
-
+print((time_after - time_before).total_seconds())
