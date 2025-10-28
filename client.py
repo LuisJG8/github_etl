8fd7aa9d2cbe33867cf58a6e98507393686f3a2a
Luis J Gonzalez <luis.glez2015@yahoo.com>
2025-10-27 20:44:19 -0400

    Refactor worker and client to use get_github_data task; update docker-compose for resource limits

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import time
 from celery.result import AsyncResult
-from worker import random_number, app 
+from worker import get_github_data, app 
 
 time.sleep(5)
 
-result_future = random_number.delay(100)
+result_future = get_github_data.delay(100)
 result = AsyncResult(result_future.id, app=app)
 
 print('Done')
--- worker.py
+++ worker.py
@@ -3,16 +3,44 @@ import time
 import random 
 
 from celery import Celery
+import os 
+import asyncio
+import time
+from datetime import datetime
+from github import Auth, Github
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = Celery(
-    'random_number',
-    broker = os.getenv('CELERY_BROKER_URL')
+    'github_repos',
+    broker = os.getenv('CELERY_BROKER_URL'),
     backend = os.getenv('CELERY_BACKEND_URL')
 )
 
-@app.task
-def random_number(max_value):
-    time.sleep(5)
-    return random.randint(0, max_value)
+api_token = os.getenv("GITHUB_API_TOKEN")
+print(api_token)
+auth = Auth.Token(api_token)
+gh = Github(auth=auth)
 
+
+repositories = gh.get_repos(since=0)
+
+
+@app.task
+def get_github_data():
+    mylist = []
+    
+    for repo in repositories:
+        print(repo)
+        print(repo.stargazers_count)
+        mylist.append(repo)
+
+        break
+    
+    return f"the name is {mylist[0] if mylist else 'No repos found'}"
+
+# @app.task
+# def random_number(max_value):
+#     time.sleep(5)
+#     return 'hello i am sponge bob'
