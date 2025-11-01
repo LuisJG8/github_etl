af3766045b59ed68ef50046df5c61b7bafdb96e6
Luis J Gonzalez <luis.glez2015@yahoo.com>
2025-11-01 19:53:12 -0400

    Enhance get_github_data function by adding detailed data points and improving Repo model structure for better clarity and functionality.

--- main.py
+++ main.py
@@ -4,16 +4,39 @@ import time
 from datetime import datetime
 from github import Auth, Github
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import List
 load_dotenv()
 
 
 
 api_token = os.getenv("GITHUB_API_TOKEN")
-print(api_token)
 auth = Auth.Token(api_token)
 gh = Github(auth=auth)
 
 time_before = datetime.now()
+ 
+class Repo(BaseModel):
+    id: int
+    name: str
+    full_name: str 
+    stars: int
+    open_issues_: List[str]
+    first_issue: str
+    updated_at: datetime
+    homepage: str 
+    allow_forking: bool
+    visibility: str 
+    topics: List[str]
+    open_issues: int
+    prog_language: str
+    watchers_count: int
+    fork_count: int
+    license: str 
+    repo_content: List[str]
+    creation_data: datetime 
+    owner_avatar: str
+    description: str 
 
 
 def github_api_data_point(repo_data_point):
@@ -26,41 +49,67 @@ def github_api_data_point(repo_data_point):
     else:
        print(repo_data_point)
        
-
     return repo_data_point
 
+repo_offset = 0
+repositories = gh.get_repos(since=repo_offset)
 
-repositories = gh.get_repos(since=0)
-def get_github_data():
+
+def get_github_data(github_data_formatted: Repo):
 
     counter = 0
     for repo in repositories:
-        # github_api_data_point(repo.full_name)
+        print("Full Name:")
+        github_api_data_point(repo.full_name)
+        print("Stargazers Count:")
         github_api_data_point(repo.stargazers_count)
+        print("Open Issues:")
         github_api_data_point(repo.open_issues)
-        # github_api_data_point(repo.topics)
-        # github_api_data_point(repo.open_issues_count)
-        # github_api_data_point(repo.language)
-        # github_api_data_point(repo.watchers_count)
-        # github_api_data_point(repo.id)
-        # github_api_data_point(repo.forks_count)
-        # github_api_data_point(repo.license)
-        # github_api_data_point(repo.get_contents("README.md"))
-        # github_api_data_point(repo.created_at)
-        # github_api_data_point(repo.name)
-        # github_api_data_point(repo.private)
-        test = github_api_data_point(repo.get_contents(""))
+        print("First Issue:")
+        github_api_data_point(repo.get_issue(number=1))
+        print("Updated At:")
+        github_api_data_point(repo.updated_at)
+        print("Homepage:")
+        github_api_data_point(repo.homepage)
+        print("Allow Forking:")
+        github_api_data_point(repo.allow_forking)
+        print("Visibility:")
+        github_api_data_point(repo.visibility)
+        print("Topics:")
+        github_api_data_point(repo.topics)
+        print("Open Issues Count:")
+        github_api_data_point(repo.open_issues_count)
+        print("Language:")
+        github_api_data_point(repo.language)
+        print("Watchers Count:")
+        github_api_data_point(repo.watchers_count)
+        print("ID:")
+        github_api_data_point(repo.id)
+        print("Forks Count:")
+        github_api_data_point(repo.forks_count)
+        print("License:")
+        github_api_data_point(repo.license)
+        print("README.md:")
+        github_api_data_point(repo.get_contents("README.md"))
+        print("Created At:")
+        github_api_data_point(repo.created_at)
+        print("Name:")
+        github_api_data_point(repo.name)
+        print("Owner User View Type:")
+        github_api_data_point(repo.owner.user_view_type)
+        print("Root Contents:")
+        github_api_data_point(type(repo.get_contents("")))
+        print("Owner Avatar URL:")
         github_api_data_point(repo.owner.avatar_url)
+        print("Description:")
         github_api_data_point(repo.description)
         
 
         counter += 1
         break
-    
-    return f" the name of the repo is actually going to be {repo.full_name}" if repo else "idk man"
 
-time_nw = datetime.now()
 
 get_github_data()
 
+time_nw = datetime.now()
 print(time_nw - time_before)
