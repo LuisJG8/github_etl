7903464014f9ed8f53169e363509301737548a54
Luis J Gonzalez <luis.glez2015@yahoo.com>
2026-01-06 22:29:56 -0500

    testing

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
 import time
 import logging
+from pathlib import Path
 from datetime import datetime
 from celery.result import AsyncResult
 from worker import get_github_data, app 
 from rb_queue.rabbitmq import consume_repos
 from pydantic_models.github import RabbitMQ_Data_Validation
+import polars as pl
+from worker import get_github_data
+import json
 
 
-celery_task_get_repos = get_github_data.delay()
-result = AsyncResult(celery_task_get_repos.id, app=app)
+# celery_task_get_repos = get_github_data.delay()
+# result = AsyncResult(celery_task_get_repos.id, app=app)
 
 print("Waiting for Celery task to complete")
 
@@ -21,23 +25,23 @@ print("Waiting for Celery task to complete")
 #     page += 1
 
 while True:
-    if result.ready():
-        try:
-            print('Getting the result')
-            res = get_github_data.delay()
-            hey = res.get()
+    try:
+        print('Getting the result')
+        res = get_github_data.apply_async()
+        hey = res.get()
+        print("The type is here", type(hey))
 
-        except Exception as e:
-            print(e)
-            break
-        
-        else:
-            print('Done. The result state of the queue', result.state)
-            print(hey)
-            # repo_data_ = consume_repos(callback = lambda repo_data: print("This is the data from the RMQ: ", repo_data))
-            # all_data_collected.append(repo_data_)
-            
+    except Exception as e:
+        print(e)
+        break
+    
     else:
-        print("Results are not ready")
-        print(result.state)
-        time.sleep(1)
+        print("this is the else")
+        print(hey)
+        # repo_data_ = consume_repos(callback = lambda repo_data: print("This is the data from the RMQ: ", repo_data))
+        # all_data_collected.append(repo_data_)
+
+        m_dir = Path("data").mkdir(parents=True, exist_ok=True)
+        maa = pl.DataFrame()
+        maa.write_parquet("data/testing.parquet", compression="zstd")
+        break
